b2f1c2930b5d8d552fb63c93bc7b60514f227930
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-06 20:51:54 +0530

    pattern

--- .vscode/day1/pattern.cpp
+++ .vscode/day1/pattern.cpp
@@ -1,7 +1,52 @@
 #include<iostream>
 using namespace std;
 
+void pattern1(int n){
+    for (int i = 1; i <= n ; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+void pattern2(int n){
+    for (int i = 1; i <= n ; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+
+void pattern3(int n){
+    for (int i = 1; i <= n ; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if(i==j){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
 
 int main() {
-    cout<<"helllo world";
+    int n;
+    cout<<"enterv the value of n "<<endl;
+    cin>>n;
+
+    //pattern1(n);
+    //pattern2(n);
+    pattern3(n);
 }
