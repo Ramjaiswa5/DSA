a1e32b275e32bbee4771bd97e1b707a4d72e01ee
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-06 23:11:40 +0530

    pattern5

--- .vscode/day1/pattern.cpp
+++ .vscode/day1/pattern.cpp
@@ -57,6 +57,38 @@ void pattern4(int n){
     }
 }
 
+void pattern5(int n){
+    int st= n/2 +1;
+    int sp= 1;
+    for (int i = 1; i <= n ; i++)
+    {
+
+        for (int j = 1; j <= st; j++)
+        {
+            cout<<"*";
+        }
+        for (int j = 1; j <= sp; j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= st; j++)
+        {
+            cout<<"*";
+        }
+
+        if (i<=n/2)
+        {
+            st--;
+            sp+=2;
+        }
+        else
+        {
+            st++;
+            sp-=2;
+        }
+        cout<<endl;
+    }
+}
 
 
 int main() {
@@ -67,5 +99,6 @@ int main() {
     //pattern1(n);
     //pattern2(n);
     //pattern3(n);
-    pattern4(n);
+    //pattern4(n);
+    pattern5(n);
 }
