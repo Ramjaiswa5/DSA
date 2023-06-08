27485e615a883ed699466d01b8c0c691c6c30915
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-08 20:32:41 +0530

    2D vector

---
+++ .vscode/day2/2D_vector.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void input(vector<vector<int>>&arr){
+    int n = arr.size();
+    int m = arr[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin>>arr[i][j];
+        }
+        
+    }
+    
+}
+
+void display(vector<vector<int>>&arr){
+    int n = arr.size();
+    int m = arr[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+int main(){
+    int n,m;
+    cout<<"enter row and coloum "<<endl;
+    cin>>n>>m;
+
+    vector<vector<int>>arr(n,vector<int>(m,0));
+    input(arr);
+    display(arr);
+}
