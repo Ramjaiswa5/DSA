bbf3551a0138e9d77e29237fa53ab3d9a819a4dc
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-09 01:38:25 +0530

    day3

--- .vscode/day3/string.cpp
+++ .vscode/day3/string.cpp
@@ -1,9 +1,43 @@
 #include<iostream>
 using namespace std;
 
+bool palindrome(string str){
+    int si =0;
+    int ei = str.length()-1;
+
+    while (si<ei)
+    {
+        if (str[si++] != str[ei--])
+        {
+            return false;
+        }
+        
+    }
+    return true;
+    
+}
+
+void getAllSubString(string str){
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int len = 1; len <= str.length(); len++)
+        {
+            cout<<(str.substr(i,len))<<endl;
+        }
+        
+    }
+    
+}
+
 int main(){
     string str;
     cout<<"enter string"<<endl;
     cin>>str;
-    cout<< str;
+    //cout<< str<<endl;
+
+    //cout<<palindrome(str)<<endl;
+
+    getAllSubString(str);
+
+    return 0;
 }
