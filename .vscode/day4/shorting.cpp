added437b4d8440d6cab0b3aed581c11b804e2b2
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-10 12:19:01 +0530

    day4

--- .vscode/day4/shorting.cpp
+++ .vscode/day4/shorting.cpp
@@ -25,10 +25,27 @@ void selectionSort(vector<int>&arr){
     
 }
 
+void insertionSort(vector<int>&arr){
+    int n =arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        int value = arr[i];
+        int j = i-1;
+
+        while(j>=0 && arr[j] > value){
+            arr[j+1] = arr[j];
+            j--; 
+        }
+        
+        arr[j+1] = value;
+    }
+}
+
 int main(){
 
     vector<int>arr ={ 2,4,5,1,9,7};
-    selectionSort(arr);
+    //selectionSort(arr);
+    insertionSort(arr);
     for(int ele: arr){
         cout<<ele<<" ";
     }
