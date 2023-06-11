51cd0c8b05855c6a807e0a31d3273dab1a6c49c5
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-11 13:34:24 +0530

    searching

--- .vscode/day4/searching.cpp
+++ .vscode/day4/searching.cpp
@@ -16,10 +16,36 @@ int linearSearch(vector<int>&arr, int data){
     
 }
 
+bool binarySearch(vector<int>&arr, int data){
+    int n = arr.size();
+    int si = 0;
+    int ei = n-1;
+
+    while(si <= ei){
+        int mid = (si+ei)/2;
+
+        if (arr[mid] == data)
+        {
+            return true;
+        }
+        else if (arr[mid]< data)
+        {
+            si = mid+1;
+        }
+        else
+        {
+            ei = mid-1;
+        }
+           
+    }
+    return false;
+}
+
 int main(){
 
-    vector<int>arr ={ 2,4,5,1,9,7};
-    cout<<linearSearch(arr, 5);
+    vector<int>arr ={ 8,2,4,5,1,9,7};
+    //cout<<linearSearch(arr, 5);
+    cout<<binarySearch(arr,8);
 
     return 0;
 }
