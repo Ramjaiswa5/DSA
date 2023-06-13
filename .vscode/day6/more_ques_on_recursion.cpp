d37c3de866d4c5cff38df157ad63e54d77dc53f2
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-13 08:55:23 +0530

    day6

--- .vscode/day6/more_ques_on_recursion.cpp
+++ .vscode/day6/more_ques_on_recursion.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int permutation(string str, string ans){
     if (str.length() == 0)
     {
         cout<<ans<<endl;
+        return 1;
     }
     int count = 0;
     for (int i = 0; i < str.length(); i++)
@@ -17,8 +19,32 @@ int permutation(string str, string ans){
     return count;
     
 }
+
+int permutationWithoutDuplicate(string str, string ans){
+    if (str.length()==0)
+    {
+        cout<<ans<<endl;
+        return 1;
+    }
+    int count = 0;
+    vector<bool>vis(26,0);
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch=str[i];
+        if (!vis[ch - 'a'])
+        {
+            vis[ch - 'a']=true;
+            string ros = str.substr(0,i) + str.substr(i+1);
+            count+=permutationWithoutDuplicate(ros,ans+ch);
+        }
+        
+    }
+    
+    return count;
+}
 int main(){
-    string str = "abc";
-    cout<<permutation(str,"");
+    string str = "aba";
+    //cout<<permutation(str,"");
+    cout<<permutationWithoutDuplicate(str,"");
     return 0;
 }
