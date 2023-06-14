2f021d0fa10dc21c98e3f3b91e0e341130cfa2c2
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-06-14 22:08:09 +0530

    day6

--- .vscode/day6/more_ques_on_recursion.cpp
+++ .vscode/day6/more_ques_on_recursion.cpp
@@ -69,12 +69,37 @@ int mazePath_HVD(int sr, int sc, int er, int ec, string ans){
     return count;
 }
 
+int mazePath_HVD_Jump(int sr, int sc, int er, int ec, string ans){
+    if (sr == er && sc == ec)
+    {
+        cout<<ans<<endl;
+        return 1;
+    }
+    int count=0;
+    // Horizontal Direction
+    for(int jump =1; sc+jump <= ec; jump++)
+    {
+        count+= mazePath_HVD_Jump(sr,sc+jump,er,ec, ans+"H" + to_string(jump));
+    }
+     // Vertical Direction
+    for(int jump =1; sr+jump <= er; jump++)
+    {
+        count+= mazePath_HVD_Jump(sr+jump,sc,er,ec, ans+"V" + to_string(jump));
+    }
+     // Diagonal Direction
+    for(int jump =1; sc+jump <= ec && sr+jump<=er; jump++)
+    {
+        count+= mazePath_HVD_Jump(sr+jump,sc+jump,er,ec, ans+"D" + to_string(jump));
+    }
+    return count;
+}
 
 int main(){
     string str = "aba";
     //cout<<permutation(str,"");
     //cout<<permutationWithoutDuplicate(str,"");
-    cout<<mazePath_HVD(0,0,2,2,"");
+    //cout<<mazePath_HVD(0,0,2,2,"");
+    cout<<mazePath_HVD_Jump(0,0,2,2,"");
 
     return 0;
 }
